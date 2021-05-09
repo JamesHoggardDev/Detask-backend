d3eb7f71966440435fe1d63dbfb148adffed81be
james <jameshoggard.dev@gmail.com>
2021-05-09 17:27:20 -0400

    added more

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,29 @@ class ProjectsController < ApplicationController
         render json: projects, except: [:created_at, :updated_at]
     end
     
+
+    def create
+        new_project = Project.create!(project_params)
+        render json: new_project 
+    end
+
+    def destroy
+        project = Project.find(params[:id])
+        project.destroy!
+        render json: {}
+    end
+
+    def update
+        project = Project.find(params[:id])
+        project.update(project_params)
+
+        render json: project
+    end
+
+    private
+
+    def project_params
+        params.permit(:title, :user_id, :notes)
+    end
+
 end
