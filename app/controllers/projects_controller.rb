f021b54f8d18a1c9d58d3808b8c8bd5bf2b7a6a3
james <jameshoggard.dev@gmail.com>
2021-05-09 14:24:08 -0400

    added setup

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,8 @@
+class ProjectsController < ApplicationController
+
+    def index
+        projects = Project.all
+        render json: projects, except: [:created_at, :updated_at]
+    end
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting Projects/Users"
+
+User.destroy_all
+Project.destroy_all
+
+3.times do
+    User.create!({
+        username: ["tyson321","shell321","mann321","popp321", "bana321"].sample,
+        password: ["1second", "2conds", "3conds","4conds"].sample
+    })
+end
+
+projects = [
+  {
+    title: "Buff the Deck",
+    user_id: 2, 
+    notes: "AAAAAAA"
+  },
+  {
+    title: "Replace Washing Machine",
+    user_id: 3, 
+    notes: "SSSSSSSSSSS"
+  },
+  {
+    title: "Clear Gutters",
+    user_id: 1, 
+    notes: "DDDDDDDDDDD"
+  },  
+  {
+    title: "Replace Bathroom Door",
+    user_id: 4, 
+    notes: "FFFFFFFFFF"
+  },
+]
+
+projects.each do |project_hash|
+    Project.create!(project_hash)
+end
+
+puts "Seeded"
